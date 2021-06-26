6375985f3f98c36c46f85a921138c8e255fd7a7a
nathanniel19 <jjuand22@gmail.com>
2021-06-26 23:52:19 +0700

    main

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/post.js';
+
+const app = express();
+
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/post', postRoutes);
+
+const CONNECTION_URL = "mongodb+srv://nathanniel19:juan22jj@cluster0.klpnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log("Server running on port: ${PORT}")))
+    .catch((error) => console.log(error.messasge));
+
+mongoose.set('useFindAndModify', false);
